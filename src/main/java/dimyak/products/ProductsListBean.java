ca8dacf8324f3b738fae78e1491bf2177a0f0a62
Dima Yakovenko <34286221+dimyak@users.noreply.github.com>
2018-07-13 10:05:39 +0300

    Table products CDI List Bean

---
+++ src/main/java/dimyak/products/ProductsListBean.java
@@ -0,0 +1,43 @@
+package dimyak.products;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class ProductsListBean implements Serializable {
+    private List<Product> products = new ArrayList<Product>();
+
+    @PostConstruct
+    private void initialize(){
+        Product product;
+
+        product= new Product();
+        product.setId("id");
+        product.setName("Food");
+        product.setPrice(100);
+        products.add(product);
+
+
+        product= new Product();
+        product.setId("id");
+        product.setName("Computer");
+        product.setPrice(10000);
+        products.add(product);
+
+
+        product= new Product();
+        product.setId("id");
+        product.setName("Telephone");
+        product.setPrice(5000);
+        products.add(product);
+    }
+
+    public List<Product> getProducts(){
+        return products;
+    }
+}
